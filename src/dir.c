017895ddbf6b2f78067440c2ae8891b5af96bdc4
KisuYami <6reberti6@gmail.com>
2019-05-04 10:29:08 -0300

    Work

--- src/dir.c
+++ src/dir.c
@@ -14,8 +14,10 @@ int listFiles(char *l[255], char *cwd) {
 
 	if(d) {
 		while((dir = readdir(d)) != NULL) {
-			l[i] = dir->d_name;
-			i++;
+			if(*dir->d_name != '.') { // Don't Show hidden files
+				l[i] = dir->d_name;
+				i++;
+			}
 		}
 
 		closedir(d);
@@ -26,16 +28,22 @@ int listFiles(char *l[255], char *cwd) {
 char *cdEnter(const char state[], const char text[]) { // Works
 
 	char *result = malloc(strlen(state) + strlen(text) + 1);
+
 	strcpy(result, state);
 	strcat(result, "/");
 	strcat(result, text);
 
 	return result;
 }
+char *cdBack(const char path[]) { // Don't do that, is wrong, but work
 
-char *cdBack(const char path[]) {// Don't work
+	char *newPath = cdEnter(path, "../");
+	return newPath;
+}
+/*
+char *cdBack(const char path[]) { // Don't work
 
-	int i, f;
+	int i, f, z;
 	char *newPath = calloc(25, 255);
 
 	while(path[i] != '\0') {
@@ -43,12 +51,15 @@ char *cdBack(const char path[]) {// Don't work
 		if(path[i] == '/')
 			f = 0;
 
+		newPath[z] = path[i];
+		z++;
 		f++;
 		i++;
 	}
 
-	newPath = realloc(newPath, (sizeof(*path) - f) + 1);
-	newPath[sizeof(newPath)] = '\0';
+	newPath = realloc(newPath, (sizeof(*path)) + 1);
+	//newPath[sizeof(newPath)] = '\0';
 
 	return newPath;
 }
+*/
