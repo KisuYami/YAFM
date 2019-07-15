29c6bb6b91fb2fb11a160b4f5bc1f91fad661b12
Reberti Soares <6reberti6@gmail.com>
2019-07-15 14:43:54 -0300

    Clear Code

--- src/dir.h
+++ src/dir.h
@@ -1,90 +1,17 @@
-/** @file dir.h
+#ifndef DIR_HEADER
+#define DIR_HEADER
 
-void previewDir(char *path, char *text) {
+#define PATH_MAX 4096 + 1 /* chars in a path name including nul(linux) */
+#define NAME_MAX 256 + 1  /* chars in a path name including nul(linux) */
 
-	int Y, previewLenght, start;
-	size_t size_result = strlen(state) + strlen(text) + CD_ENTER_BUFFER_SIZE;
-	char *previewList[LIST_LENGHT];
-	char *previewPwd = malloc(size_result);
+struct working_dir {
+  int nm_files;         // A count for the files in the working folder
+  char *file[NAME_MAX]; // This should be used with malloc
+  char path[PATH_MAX];
+};
 
-	snprintf(previewPwd, size_result, "%s/%s", state, text);
+int list_files(struct working_dir *new_dir);
+void cd_enter(struct working_dir *new_dir, int cursor);
+void cd_back(struct working_dir *new_dir);
 
-	Y = getmaxy(stdscr);
-	previewLenght = listFiles(previewList, previewPwd);
-
-	start = (Y / 2) + 3;
-
-	if(isFile(previewPw) != 1)
-		displayFiles(previewList, previewLenght, -1, start);
-	else
-		return;
-}
-  @brief Folder and File Operations
-
-  This code make the core functions like enter and exiting folders, using
-  editing files, and opening files with external source.
-  */
-/** @fn int openFile(char path[], const char text[])
-  @brief Open file with defined programs
-
-  This function get the path of file as argument, and test the
-  extension against files_extensions, only files with extensions should
-  be tested.
-
-  @author Reberti Soares
-  @date 05-05-2019
-  */
-/** @fn int isFile(const char *path);
-  @brief Check the type of file
-
-  @param *path Path where check is made.
-  Check using POSIX compliant system call, if a file is a regular file.
-  @author Frédéric Hamidi(frederic.hamidi@gmail.com)
-  @date 05-05-2019
-  */
-/** @fn int listFiles(char *l[255], char *cwd);
-  @brief Get files in path
-
-  @param *l[] List where result well be put.
-  @param *cwd Current Working Dir.
-
-  @author Reberti Soares
-  @date 05-05-2019
-  */
-/** @fn char *cdBack(char path[]);
-  @brief Change Dir in ../
-
-  @param path[] Path to go.
-  @author Reberti Soares
-  @date 05-05-2019
-  */
-/** @fn char *cdEnter(char state[], const char text[]);
-  @brief Open dir or file
-
-  This function enter in a dir, or open a file if the var text
-  points to one. See openFile function
-  @param state[] Working dir.
-  @param text[] Path to go.
-
-  @author Reberti Soares
-  @date 05-05-2019
-  */
-/** @fn void shellActions(char path[], char file[], char shellCommand[], char special[]);
-  @brief Use a editor for action
-
-  @param path[] Where the file is.
-  @param file[] The file itself(can be empty string).
-  @param command[] Command executed in file(can be empty string).
-  @param special[] Some statements for command executed in file(can be empty string).
-
-  @author Reberti Soares
-  @date 05-05-2019
-  */
-int openFile(char path[]);
-int isFile(char *path); // I see it
-int listFiles(char *l[], char *cwd);
-void cdEnter(char *path, const char workingFile[]);
-void cdBack(char *path);
-void shellActions(char path[], char file[], const char shellCommand[], char special[]);
-void bulkRename(char path[]);
-void previewDir(char *path, char *text);
+#endif
--- src/display.c
+++ src/display.c
@@ -1,51 +1,21 @@
+#include "../config.h"
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
-#include "dir.h"
-#include "../config.h"
 
-void setupScr() {
+void setup_scr() {
 
-	initscr();
-	cbreak();
-        noecho();
-	curs_set(0); start_color();
-	//Colors
-	init_pair(1, COLOR_RED,  COLOR_BLACK);
-	init_pair(2, COLOR_CYAN, COLOR_BLACK);
-	init_pair(3, COLOR_BLUE, COLOR_BLACK);
+  initscr();
+  cbreak();
+  noecho();
+  curs_set(0);
+  start_color();
+  // Colors
+  init_pair(1, COLOR_RED, COLOR_BLACK);
+  init_pair(2, COLOR_CYAN, COLOR_BLACK);
+  init_pair(3, COLOR_BLUE, COLOR_BLACK);
 }
 
-void displayFiles(char *path, char *list[LIST_LENGHT], int lenght, int cursor, int startPoint) {
-
-	char **tmpList = list;
-	size_t size_pathFile = sizeof(char) * (256 + sizeof(*path));
-	char fullFile[size_pathFile];
-
-	for(int i = 0; i < lenght; i++) {
-
-		snprintf(fullFile, size_pathFile, "%s/%s", path, tmpList[i]);
-
-		if(i == cursor) {
-
-			attron(A_STANDOUT | COLOR_PAIR(1));
-			mvprintw(i+1, startPoint, "%s", tmpList[i]);
-			attroff(A_STANDOUT | COLOR_PAIR(1));
-
-		} else if(isFile(fullFile) != 0 && COLORED_FILES == 0) {
-
-			attron(A_BOLD | COLOR_PAIR(3));
-			mvprintw(i+1, startPoint, "%s", tmpList[i]);
-			attroff(A_BOLD | COLOR_PAIR(3));
-
-		} else
-			mvprintw(i+1, startPoint, "%s", tmpList[i]);
-	}
-}
-
-void displayDirPath(char *path) {
-
-	attron(A_UNDERLINE | COLOR_PAIR(2));
-	mvprintw(0, 0, "%s", path);
-	attroff(A_UNDERLINE | COLOR_PAIR(2));
-}
+void display_path(/*TODO*/) {}
+void display_files(/*TODO*/) {}
+void display_preview(/*TODO*/) {}
