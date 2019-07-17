570685adfef278562406b26f0a9b871f3917bb22
Reberti Soares <6reberti6@gmail.com>
2019-07-16 21:55:31 -0300

    All functions Working
    Code made from scratch, now clear
    Add yank, kill and paste
    Add delete files
    Add preview

--- config.h
+++ config.h
@@ -1,4 +1,14 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef CONFIG_HEADER_FILE
+#define CONFIG_HEADER_FILE
 
+#define KEY_QUIT 'q'
+#define KEY_MOV_UP 'k'
+#define KEY_MOV_DOWN 'j'
+#define KEY_MOV_LEFT 'h'
+#define KEY_MOV_RIGHT 'l'
+#define KEY_FILE_DEL 'x'
+#define KEY_FILE_KILL 'd'
+#define KEY_FILE_YANK 'y'
+#define KEY_FILE_PASTE 'p'
+#define CLIPBOARD_MAX 50
 #endif
--- src/main.c
+++ src/main.c
@@ -1,3 +1,39 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2019, Reberti Carvalho Soares
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
+TODO:
+ - [ ] Directory Preview
+*/
 #include "../config.h"
 #include "dir.h"
 #include "display.h"
@@ -9,26 +45,91 @@
 
 int main() {
 
-  struct working_dir new_dir;
-  list_files(&new_dir);
+  int x, y;
+  int curs;
+  int clipboard_number;
+  char key;
+  WINDOW *left;
+  struct working_dir main_dir;
 
-  printf("%s\n", new_dir.path);
-  for (int i = 0; i < new_dir.nm_files; i++) {
-    printf("%s\n", new_dir.file[i]);
+  // Basic Setup
+  screen_setup();
+  getmaxyx(stdscr, y, x);
+  left = newwin(x / 2, y, 0, 0);
+
+  // Setup window(s)
+  clear();
+  wclear(left);
+  file_list(&main_dir);
+  display_path(&main_dir, left);
+  display_files(&main_dir, left, curs);
+  wrefresh(left);
+
+  // Main loop
+  while ((key = getchar()) != KEY_QUIT) {
+    switch (key) {
+    case KEY_MOV_UP:
+      if (curs >= 1)
+        curs--;
+      break;
+
+    case KEY_MOV_DOWN:
+      if (curs <= main_dir.num_files - 2)
+        curs++;
+      break;
+
+    case KEY_MOV_LEFT:
+      cd_back(&main_dir);
+      break;
+
+    case KEY_MOV_RIGHT:
+      cd_enter(&main_dir, curs);
+      break;
+
+    case KEY_FILE_DEL:
+      file_delete(&main_dir, left, curs);
+      break;
+
+    case KEY_FILE_KILL:
+      file_kill_yank(&main_dir, 0, curs, clipboard_number);
+      clipboard_number++;
+      break;
+
+    case KEY_FILE_YANK:
+      file_kill_yank(&main_dir, 1, curs, clipboard_number);
+      clipboard_number++;
+      break;
+
+    case KEY_FILE_PASTE:
+      file_paste(&main_dir);
+      clipboard_number = 0;
+      break;
+
+    default:
+      break;
+    }
+    // If terminal size has changed, update the window size
+    if (is_term_resized(y, x) == true) {
+
+      delwin(left);
+      getmaxyx(stdscr, y, x);
+      left = newwin(x / 2, y, 0, 0);
+    }
+
+    if (curs >= main_dir.num_files || curs < 0)
+      curs = 0;
+
+    // Updating Screen
+    wclear(left);
+
+    display_path(&main_dir, left);
+    display_files(&main_dir, left, curs);
+
+    wrefresh(left);
   }
 
-  cd_enter(&new_dir, 4);
-
-  printf("\n\n%s\n", new_dir.path);
-  for (int i = 0; i < new_dir.nm_files; i++) {
-    printf("%s\n", new_dir.file[i]);
-  }
-
-  cd_back(&new_dir);
-
-  printf("\n\n%s\n", new_dir.path);
-  for (int i = 0; i < new_dir.nm_files; i++) {
-    printf("%s\n", new_dir.file[i]);
-  }
+  // Cleaning Shit
+  delwin(left);
+  endwin();
   return 0;
 }
